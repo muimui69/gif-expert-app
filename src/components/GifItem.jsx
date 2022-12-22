fa75b15b824171f30d3c69036616983b98b7a035
muimui69 <moises_c2015@outlook.es>
2022-12-22 22:29:26 +0000

    first commit

---
+++ src/components/GifItem.jsx
@@ -0,0 +1,9 @@
+export const GifItem = ({title,url}) => {
+  console.log(title);
+  return(
+	<div className='card'>	
+		<img src={url} alt={title}></img>
+		<p>{title}</p>
+	</div>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//...archivo barril
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
